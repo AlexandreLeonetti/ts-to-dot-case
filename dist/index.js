7af3213f759e2d4093e8987f423f350111082f9a
AlexandreLeonetti <al.leo@wanadoo.fr>
2024-04-25 18:32:57 +0200

    main

---
+++ dist/index.js
@@ -0,0 +1,11 @@
+import toSnakeCase from 'to-snake-case';
+/**
+ * Convert a `string` to constant case.
+ *
+ * @param {string} string - The string to convert to constant case.
+ * @return {string} The constant-cased string.
+ */
+function toConstantCase(string) {
+    return toSnakeCase(string).toUpperCase();
+}
+export default toConstantCase;
---
+++ readme.md
@@ -0,0 +1,19 @@
+# to dot case using TypeScript 
+
+> to dot case using TypeScript. 
+
+## Install
+
+```sh
+npm install @leonetti/ts-to-dot-case
+```
+
+## Usage
+
+```js
+import {toDotCase} from '@leonetti/ts-to-dot-case';
+
+toDotCase('camelCase')   // "camel.case"
+
+
+```
